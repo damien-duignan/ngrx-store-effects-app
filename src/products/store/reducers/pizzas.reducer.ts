c1fc85d1f765e3c9124ffbfd0febaec6ebf65c88
Damien Duignan <damien.duignan1@gmail.com>
2018-11-13 10:15:54 +0100

    Optimizing Data Structures with Entities

--- src/products/store/reducers/pizzas.reducer.ts
+++ src/products/store/reducers/pizzas.reducer.ts
@@ -3,13 +3,13 @@ import { Pizza } from '../../models/pizza.model';
 import { state } from '@angular/animations';
 
 export interface PizzaState {
-  data: Pizza[],
+  entities: { [id: number]: Pizza}
   loaded: boolean,
   loading: boolean
 }
 
 export const initialState: PizzaState = {
-  data: [],
+  entities: { },
   loaded: false,
   loading: false
 }
@@ -26,12 +26,24 @@ export function reducer(state = initialState, action: fromPizzas.PizzasAction): 
     }
 
     case fromPizzas.LOAD_PIZZAS_SUCCESS: {
-      const data = action.payload;
+      const pizzas = action.payload;
+
+      const entities = pizzas.reduce((entities: { [id: number]: Pizza}, pizza: Pizza) => {
+          return {
+            ...entities,
+            [pizza.id]: pizza
+          }
+        },
+        {
+          ...state.entities
+        }
+      );
+
       return {
         ...state,
         loading: false,
         loaded: true,
-        data
+        entities,
       }
     }
 
@@ -50,4 +62,4 @@ export function reducer(state = initialState, action: fromPizzas.PizzasAction): 
 
 export const getPizzasLoading = (state: PizzaState) => state.loading;
 export const getPizzasLoaded = (state: PizzaState) => state.loaded;
-export const getPizzas = (state: PizzaState) => state.data;
+export const getPizzaEntities = (state: PizzaState) => state.entities;
